5208a89ee68aeba2f5d73f90ed5b8d898ff5ec73
Ankit Sharma <ankitsm08@gmail.com>
2024-11-02 20:57:35 +0530

    update 0.1.0

--- searchindex.en.js
+++ searchindex.en.js
@@ -89,8 +89,8 @@ var relearn_searchindex = [
   },
   {
     "breadcrumb": "",
-    "content": "This is my new home page.",
-    "description": "This is my new home page.",
+    "content": "📚 Welcome to Ank’s Ultimate Study Hub 🚀 Welcome to the mind palace of notes, formulas, hacks, and straight-up wizardry 🎉! Whether you’re battling JEE, wrestling with Class 11 \u0026 12 topics, or diving into some tasty code, you’ve found your spot. Here, everything’s laid out like a cheat sheet 📘 to life: physics, chemistry, math formulas, programming tricks, and the oh-so-sneaky shortcuts that’ll make you wonder why everyone else finds it so hard.\nOpen(still working on that), free, and overflowing with stuff you’ll actually use – feel free to binge, bookmark, and bless your exams with the power moves stored right here. Study smart, learn faster, and leave ‘em wondering what your secret is! 💡🔥\n…But I am a pirate !! 🏴‍☠️\rAhoy, matey! Welcome t’ Ank’s Treasure Trove o’ Knowledge! 💎\nYe be sailin’ in the right waters if yer after secrets t’ conquer the high seas o’ exams! From formulas and magic scrolls fer JEE battles to sneaky tricks fer ye everyday curriculum in Class 11 an’ 12, this be the hoard ye seek. Ye’ll find the finest charts fer physics, chemistry, math, an’ even some coded enchantments fer ye clever pirates. 🧠\nPlunder freely, me hearty – no coin needed! Binge it all, stow it fer later, an’ give yer mind the compass it needs t’ steer straight fer success. Now, go forth an’ show them landlubbers who rules the waves! ⚓️💀",
+    "description": "Documentation on various topics, formula sheets, extensive lists, competetive tricks, and many more.",
     "tags": [],
     "title": "Ank Docs",
     "uri": "/ank-docs/index.html"
